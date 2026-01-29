0ae8d9f47c5d49c61c50b1dcd575c43d0876ef54
Your Name <yourgmail address>
2026-01-29 16:46:18 +0530

    Added ad-glue-job.py and deploy-glue.yaml

---
+++ scripts/ad-glue-job.py
@@ -0,0 +1,159 @@
+import sys
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+from pyspark.sql.functions import col, coalesce, lower, trim, when, lit, current_timestamp
+
+args = getResolvedOptions(sys.argv, [
+    'JOB_NAME',
+    'INGESTION_DATE',
+    'RAW_DB',
+    'RAW_TABLE',
+    'PROCESSED_BUCKET',
+    'PROCESSED_DB',
+    'PROCESSED_TABLE'
+])
+
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+
+spark.conf.set("spark.sql.catalog.iceberg_catalog", "org.apache.iceberg.spark.SparkCatalog")
+spark.conf.set("spark.sql.catalog.iceberg_catalog.warehouse", args['PROCESSED_BUCKET'])
+spark.conf.set("spark.sql.catalog.iceberg_catalog.catalog-impl", "org.apache.iceberg.aws.glue.GlueCatalog")
+spark.conf.set("spark.sql.catalog.iceberg_catalog.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
+
+
+raw_df = spark.read.table(f"{args['RAW_DB']}.{args['RAW_TABLE']}").filter(col("ingestion_date") == args['INGESTION_DATE'])
+
+
+cleaned_df = (
+    raw_df
+    .withColumn("ad_cost", coalesce(col("spend"), col("cost")))
+    .withColumn("device", lower(col("device")))
+    .withColumn("country_code",
+                when(col("country_code").isin(["USA", "US"]), "US")
+                .when(col("country_code").isin(["UK", "GB"]), "GB")
+                .when(col("country_code").isin(["CAN", "CA"]), "CA")
+                .otherwise(col("country_code")))
+    .withColumn("campaign_name",
+                when(col("campaign_name").isNull(), lit("Unknown"))
+                .otherwise(col("campaign_name")))
+)
+
+enriched_df = (
+    cleaned_df
+    .withColumn("ctr", 
+                when(col("impressions") > 0, (col("clicks") / col("impressions") * 100))
+                .otherwise(lit(0)))
+    .withColumn("cpc",
+                when(col("clicks") > 0, col("ad_cost") / col("clicks"))
+                .otherwise(lit(None)))
+    .withColumn("cpa",
+                when(col("conversions") > 0, col("ad_cost") / col("conversions"))
+                .otherwise(lit(None)))
+    .withColumn("roas",
+                when(col("ad_cost") > 0, col("revenue") / col("ad_cost"))
+                .otherwise(lit(0)))
+    .withColumn("conversion_rate",
+                when(col("clicks") > 0, (col("conversions") / col("clicks") * 100))
+                .otherwise(lit(0)))
+
+    .withColumn("is_reconciled", lit(False))
+    .withColumn("reconciliation_count", lit(0))
+    .withColumn("processed_at", current_timestamp())
+)
+
+
+final_df = enriched_df.select(
+    "campaign_id",
+    "ad_platform",
+    "event_date",
+    "campaign_name",
+    "device",
+    "country_code",
+    "ad_cost",
+    "impressions",
+    "clicks",
+    "conversions",
+    "revenue",
+    "ctr",
+    "cpc",
+    "cpa",
+    "roas",
+    "conversion_rate",
+    "is_reconciled",
+    "reconciliation_count",
+    "processed_at",
+)
+
+final_df.createOrReplaceTempView("final_df")
+
+# ---- ICEBERG table creation ----
+
+spark.sql(f"""
+CREATE TABLE IF NOT EXISTS iceberg_catalog.{args['PROCESSED_DB']}.{args['PROCESSED_TABLE']} (
+    campaign_id INT,
+    ad_platform STRING,
+    event_date DATE,
+    campaign_name STRING,
+    device STRING,
+    country_code STRING,
+    ad_cost DECIMAL(10,2),
+    impressions BIGINT,
+    clicks BIGINT,
+    conversions BIGINT,
+    revenue DECIMAL(10,2),
+    ctr DECIMAL(10,4),
+    cpc DECIMAL(10,2),
+    cpa DECIMAL(10,2),
+    roas DECIMAL(10,2),
+    conversion_rate DECIMAL(10,4),
+    is_reconciled BOOLEAN,
+    reconciliation_count INT,
+    processed_at TIMESTAMP
+)
+USING iceberg 
+PARTITIONED BY (days(event_date))
+LOCATION '{args['PROCESSED_BUCKET']}/{args['PROCESSED_TABLE']}/'
+TBLPROPERTIES (
+    'format-version'='2',
+    'write.format.default'='parquet',
+    'write.parquet.compression-codec'='snappy'
+)
+""")
+
+# ---- ICEBERG table insert/update ----
+
+spark.sql(f"""
+MERGE INTO iceberg_catalog.{args['PROCESSED_DB']}.{args['PROCESSED_TABLE']} t
+USING final_df s
+ON t.campaign_id = s.campaign_id
+   AND t.ad_platform = s.ad_platform
+   AND t.event_date = s.event_date
+   AND t.event_date >= DATE('{args['INGESTION_DATE']}') - INTERVAL '3' DAY
+
+WHEN MATCHED THEN UPDATE SET 
+    ad_cost = s.ad_cost,
+    impressions = s.impressions,
+    clicks = s.clicks,
+    conversions = s.conversions,
+    revenue = s.revenue,
+    ctr = s.ctr,
+    cpc = s.cpc,
+    cpa = s.cpa,
+    roas = s.roas,
+    is_reconciled = TRUE,
+    conversion_rate = s.conversion_rate,
+    reconciliation_count = t.reconciliation_count + 1,
+    processed_at = s.processed_at
+          
+WHEN NOT MATCHED THEN INSERT *
+""")
+
+job.commit()
